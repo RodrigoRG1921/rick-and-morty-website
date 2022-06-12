c2c3895fecbd83a20392cd4835a6accd6a4410ff
RodrigoRG1921 <rodrigo1921r@gmail.com>
2022-06-12 11:57:31 -0500

    Imported Characters page

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import {
 
 import SplashScreen from './screens/splashscreen'
 import HomeScreen from './screens/home'
+import Characters from './screens/characters'
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path='/' element={ <SplashScreen /> } />
       <Route path='home' element={ <HomeScreen /> } />
+      <Route path='characters' element={ <Characters /> } />
     </Routes>
   </BrowserRouter>
 )
