5ba05d155b7ec93f01f2c8fa3612c388e5ccef21
RodrigoRG1921 <rodrigo1921r@gmail.com>
2022-06-12 13:25:47 -0500

    added character screen

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 import SplashScreen from './screens/splashscreen'
 import HomeScreen from './screens/home'
 import Characters from './screens/characters'
+import Character from './screens/character'
 
 const App = () => (
   <BrowserRouter>
@@ -14,6 +15,7 @@ const App = () => (
       <Route path='/' element={ <SplashScreen /> } />
       <Route path='home' element={ <HomeScreen /> } />
       <Route path='characters' element={ <Characters /> } />
+      <Route path='characters/:id' element={ <Character /> } />
     </Routes>
   </BrowserRouter>
 )
