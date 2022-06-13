e8dd3de8484fa91b105d315af3966e8d3d41ea98
RodrigoRG1921 <rodrigo1921r@gmail.com>
2022-06-12 22:02:01 -0500

    Added getAllCharacters function

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,5 @@
 const Api = (() => {
   const apiBaseUrl = 'https://rickandmortyapi.com/api'
-
   const getLocationsByPagination = async ({
     page
   }) => {
@@ -37,10 +36,20 @@ const Api = (() => {
     }
   }
 
+  const getAllCharacters = async (page) => {
+    try{
+      const response = await fetch(`${apiBaseUrl}/character?page=${page}`)
+      return await response.json()
+    } catch(error){
+      console.error(error)
+    }
+  }
+
   return {
     getLocationsByPagination,
     getLocationById,
-    getMultipleCharacters
+    getMultipleCharacters,
+    getAllCharacters
   }
 })()
 
