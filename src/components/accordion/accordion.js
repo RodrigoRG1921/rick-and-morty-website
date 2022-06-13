3c276d037736a06852cbe7024b82a897c17b25d2
RodrigoRG1921 <rodrigo1921r@gmail.com>
2022-06-13 02:25:20 -0500

    imported css

--- src/components/accordion/accordion.js
+++ src/components/accordion/accordion.js
@@ -5,6 +5,7 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import './accordion.css'
 
 const Trait = ({
   title,
